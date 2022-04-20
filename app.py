1fe02324129aefdc8eced744092aa34b86cce7d1
Cristian Fartade <fartade.cristian@gmail.com>
2022-04-20 16:01:42 +0300

    1

--- app.py
+++ app.py
@@ -21,33 +21,7 @@ def translate(value):
     response = trs.translate(value)
     print(response[0])
     return render_template('translate.html', result=response[0])
-    # def getconn():
-    #     conn = connector.connect(
-    #         "cc-tema3-345518:europe-north1:database",
-    #         "pymysql",
-    #         user="root",
-    #         password="12345",
-    #         db="tema3-db"
-    #     )
-    #     return conn
-    #
-    # pool = sqlalchemy.create_engine(
-    #     "mysql+pymysql://",
-    #     creator=getconn,
-    # )
-    #
-    # result = translate_text('ro', value)
-    # original = value
-    # translated = result['translatedText']
-    # fromLanguage = result['detectedSourceLanguage']
-    # toLanguage = 'ro'
-    #
-    # # query or insert into Cloud SQL database
-    # with pool.connect() as db_conn:
-    #     # query database
-    #     query_result = db_conn.execute(f"INSERT INTO translate (original, translated, from_lang, to_lang) VALUES (\"{original}\", \"{translated}\", \"{fromLanguage}\", \"{toLanguage}\")")
-    #
-    # return render_template('translate.html', result=result, db='Translation added to database')
+
 
 
 if __name__ == '__main__':
