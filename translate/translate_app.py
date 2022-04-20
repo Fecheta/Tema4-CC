d959314c7aa8739fc61b3eea3ecc0581e07c9e5b
Cristian Fartade <fartade.cristian@gmail.com>
2022-04-20 16:22:04 +0300

    fin

--- translate/translate_app.py
+++ translate/translate_app.py
@@ -5,23 +5,12 @@ from translate.translate import Translate
 app = Flask(__name__)
 
 
-@app.route('/')
-def start_page():
-    return 'Ana are mere'
-
-
-@app.route('/<int:count>')
-def second_page(count):
-    return render_template('index.html', no=range(count))
-
-
 @app.route('/translate/<value>')
 def translate(value):
     trs = Translate()
     response = trs.translate(value)
     print(response[0])
     return render_template('translate.html', result=response[0])
-
 
 
 if __name__ == '__main__':
